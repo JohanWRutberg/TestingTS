f8117b549cdd64e3ca36be9b9aae6c5bd3520d60
JoRuPeRu <johan.rutberg@iths.se>
2023-12-13 22:36:57 +0100

    Create heading - Function

---
+++ headingCreator/headingCreator.ts
@@ -0,0 +1,9 @@
+function createHeading(text: string, level: number): string {
+  if (level < 1 || level > 6) {
+    throw new Error("Heading level must be between 1 and 6");
+  }
+
+  return `<h${level}>${text}</h${level}>`;
+}
+
+export default createHeading;
