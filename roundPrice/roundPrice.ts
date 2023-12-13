5e356684eb6aebc4d28baf4cecf0d37b26f66ca3
JoRuPeRu <johan.rutberg@iths.se>
2023-12-13 23:23:04 +0100

    Format price - Function B

--- roundPrice/roundPrice.ts
+++ roundPrice/roundPrice.ts
@@ -1,11 +1,15 @@
-function roundPrice(price: number, currencyPattern: string = "%PRICE% SEK"): string {
+function roundPrice(price: number, options: { currencyPattern?: string; currency?: string } = {}): string {
+  const { currencyPattern = "%PRICE% %CURRENCY%", currency = "SEK" } = options;
+
   const thirdDecimal = Math.floor((price * 1000) % 10);
 
   const roundedPrice = thirdDecimal >= 5 ? Math.ceil(price * 100) / 100 : Math.floor(price * 100) / 100;
 
   const formattedPrice = roundedPrice.toFixed(2);
 
-  return currencyPattern.replace("%PRICE%", formattedPrice);
+  const formattedPattern = currencyPattern.replace("%PRICE%", formattedPrice).replace("%CURRENCY%", currency);
+
+  return formattedPattern;
 }
 
 export default roundPrice;
