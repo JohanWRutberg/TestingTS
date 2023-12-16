82b439d5bf67553044b7e555b461b04712e0a02a
JoRuPeRu <johan.rutberg@iths.se>
2023-12-16 13:28:45 +0100

    fix, minor explenations added to some code.

--- isPrimeNumber/isPrimeNumber.ts
+++ isPrimeNumber/isPrimeNumber.ts
@@ -3,6 +3,9 @@ function isPrimeNumber(num: number): boolean {
     return false;
   }
 
+  /* Använder en for-loop för att iterera över möjliga delare (i) från 2 upp till roten av num. */
+  /* Inuti loopen kontrolleras om num är jämnt delbart med i. Om det är det så returneras false,
+  vilket visar att num inte är ett primtal. */
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
       return false;
--- roundPrice/roundPrice.ts
+++ roundPrice/roundPrice.ts
@@ -1,12 +1,22 @@
+/* ###################################################################################################################### */
+/* Vet inte hur det var tänkt här, men gjorde 4a, 4b och 4c i samma fil och inte i olika. Detta är resultatet av samtliga */
+/* ###################################################################################################################### */
+
 function roundPrice(price: number, options: { pattern?: string; currency?: string } = {}): string {
+  /* Destrukturerar options objektet för att extrahera pattern och currency */
+  /* Om pattern eller currency inte finns i options, används standardvärdena "%PRICE% %CURRENCY%" och "SEK" */
   const { pattern = "%PRICE% %CURRENCY%", currency = "SEK" } = options;
 
+  /* Multiplicerar price med 1000 och tar modulus 10 för att få den tredje decimalen */
   const thirdDecimal = Math.floor((price * 1000) % 10);
 
+  /* Om den tredje decimalen är 5 eller högre, använd Math.ceil för att avrunda uppåt, annars använd Math.floor för att avrunda nedåt */
   const roundedPrice = thirdDecimal >= 5 ? Math.ceil(price * 100) / 100 : Math.floor(price * 100) / 100;
 
+  /* Använder toFixed(2) för att formatera roundedPrice till två decimaler */
   const formattedPrice = roundedPrice.toFixed(2);
 
+  /* Använder pattern.replace för att ersätta placeholders %PRICE% och %CURRENCY% med formattedPrice och currency */
   const formattedOutput = pattern.replace("%PRICE%", formattedPrice).replace("%CURRENCY%", currency);
 
   return formattedOutput;
